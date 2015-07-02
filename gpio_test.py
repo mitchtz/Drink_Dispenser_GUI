597a20d4b34b11f82d581d7191f253ab80367489
Mitchtz <mitchtz@gmail.com>
2015-07-02 11:49:20 -0600

    Added test file for GPIO pins

---
+++ gpio_test.py
@@ -0,0 +1,29 @@
+#Test file for function that control gpio pins on rpi
+
+##import RPi.GPIO as GPIO #To interface with Raspberry Pis GPIO pins
+import time #To sleep program
+#Get pin to test on
+pin = input("Pin to interface with: ")
+
+done = False
+while not done:
+	#Get time in milliseconds to run pump
+	pump_time = input("Time to pump (in milliseconds): ")
+	#Check for done or exit
+	if (pump_time == "done") or (pump_time == "exit"):
+		done = True
+	#Otherwise run pump
+	else:
+		#Setup output pin, default turn on is low voltage
+		##GPIO.setup(pin, GPIO.OUT)
+		
+	    #Set GPIO pin to high voltage
+	    #Should turn pump on
+		##GPIO.output(pin, True)
+
+	    #Wait specified time before turning off
+		time.sleep(int(pump_time))
+
+	    #Set GPIO pin to low voltage
+		#Should turn pump off
+		##GPIO.output(pin, False)
